03d5d3ab19fb728a8f8681b90bcdc6c653d95104
kopihannan <kopihannan3@gmail.com>
2022-10-31 18:46:27 +0600

    responsive design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,18 +19,35 @@ const Header = () => {
 
     return (
         <div>
-            <div className="navbar bg-base-100 navigation-menu py-3 bg-light shadow-lg px-24">
-                <div className="flex-1">
-                    <img className='w-11' src={logo} alt="" />
-                    <Link to='/' className=" font-bold text-2xl text-orange-500">WEB STUDIO.</Link>
+            {/* new updata nabar */}
+            <div className="navbar bg-slate-50 px-20 shadow-xl py-4">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <Link to='/course' className='btn btn-ghost font-bold text-blue-700'>Courses</Link>
+                            <Link className='btn btn-ghost font-bold text-blue-700'>Blog</Link>
+                            <Link className='btn btn-ghost font-bold text-blue-700'>Faq</Link>
+                        </ul>
+                    </div>
+                    <div className=' sm:flex items-center'>
+                        <img className='w-11 pr-2' src={logo} alt="" />
+                        <Link to='/' className=" font-bold text-2xl text-blue-700">WEB STUDIO.</Link>
+                    </div>
                 </div>
-                <div className=''>
-                    <Link to='/course' className='btn btn-ghost font-semibold text-orange-500'>Courses</Link>
-                    <Link className='btn btn-ghost font-semibold text-orange-500'>Blog</Link>
-                    <Link className='btn btn-ghost font-semibold text-orange-500'>Faq</Link>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+                        <Link to='/course' className='btn btn-ghost font-bold text-blue-700'>Courses</Link>
+                        <Link className='btn btn-ghost font-bold text-blue-700'>Blog</Link>
+                        <Link className='btn btn-ghost font-bold text-blue-700'>Faq</Link>
+                    </ul>
+                </div>
+                <div className="navbar-end">
                     {
                         user?.email ? <div className='flex pr-4'>
-                            <Link onClick={handleLogOut} className='btn btn-ghost font-semibold text-orange-500'>Logout</Link>
+                            <Link onClick={handleLogOut} className='btn btn-ghost font-bold text-blue-700'>Logout</Link>
                             <div className="flex-none">
                                 <div className="dropdown dropdown-end">
                                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar tooltip tooltip-bottom" data-tip={user?.displayName}>
@@ -41,19 +58,16 @@ const Header = () => {
                                     </label>
                                 </div>
                             </div>
-                        </div> : <Link to='/login' className='btn btn-ghost font-semibold text-orange-500'>Login</Link>
+                        </div> : <Link to='/login' className='btn btn-ghost font-bold text-blue-700'>Login</Link>
                     }
-
-                </div>
-
-
-
-                <div className="form-control">
-                    <label className="label cursor-pointer">
-                        <input type="checkbox" className="toggle toggle-primary" />
-                    </label>
+                    <div className="form-control">
+                        <label className="label cursor-pointer">
+                            <input type="checkbox" className="toggle toggle-primary" />
+                        </label>
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 };
--- src/components/Pages/CourseSide/LeftSide.js
+++ src/components/Pages/CourseSide/LeftSide.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 const LeftSide = () => {
@@ -15,16 +16,18 @@ const LeftSide = () => {
     }, [])
 
     return (
-        <div className='min-h-screen bg-base-200 grid grid-cols-2 justify-center pl-24 gap-5 py-20'>
+        <div className='min-h-screen bg-base-200 justify-center pl-24 gap-5 py-20 sm:grid grid-cols-2'>
             {
                 itm.map(d => <div className='' key={d.id}>
                     <div className=''>
-                        <div className="card card-compact  w-80 bg-base-100 shadow-xl">
+                        <div className="card card-compact my-6 w-80 bg-base-100 shadow-xl">
                             <figure><img className='w-80 h-52' src={d.image} alt="Shoes" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">{d.title}</h2>
-                                <div className="card-actions justify-end">
-                                    <button className="btn btn-success">Buy Now</button>
+                                <h2 className="text-lg font-semibold">Price: ${d.price}</h2>
+                                <div className="card-actions justify-between ">
+                                    <Link className="btn btn-warning">View Details</Link>
+                                    <Link className="btn btn-success">Buy Now</Link>
                                 </div>
                             </div>
                         </div>
