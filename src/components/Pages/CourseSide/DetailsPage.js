47955d1a240b9b13bcbb0ff2775bec6829a7086b
kopihannan <kopihannan3@gmail.com>
2022-10-31 15:50:51 +0600

    genarate pdf file

--- src/components/Pages/CourseSide/DetailsPage.js
+++ src/components/Pages/CourseSide/DetailsPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
 
 
 
@@ -7,19 +9,27 @@ const DetailsPage = () => {
 
     const courses = useLoaderData()
 
- 
+
 
     return (
-        <div className='min-h-screen bg-base-200 flex justify-center'>
-            <div className='w-3/4 py-5'>
-                <img className='w-2/3 h-96 rounded m-auto' src={courses.image} alt="" />
-                <h2 className='py-4 text-5xl font-semibold text-orange-500'>{courses.title}</h2>
-                <p className='font-medium text-justify'>{courses.description}</p>
-                <div className='text-center py-10'>
-                    <Link  to={`/checkout/${courses.id}`} className='btn btn-info'>Get Premium Access</Link>
-                </div>
+        <div className='min-h-screen bg-base-200'>
+            <div className='flex justify-end pr-24 py-4'>
+                <Pdf targetRef={ref} filename="code-example.pdf">
+                    {({ toPdf }) => <button className='btn btn-warning font-medium' onClick={toPdf}>Generate Pdf</button>}
+                </Pdf>
             </div>
-            
+            <div className='min-h-screen bg-base-200 flex justify-center'>
+
+                <div className='w-3/4 py-5' ref={ref}>
+                    <img className='w-2/3 h-96 rounded m-auto' src={courses.image} alt="" />
+                    <h2 className='py-4 text-5xl font-semibold text-orange-500'>{courses.title}</h2>
+                    <p className='font-medium text-justify'>{courses.description}</p>
+                    <div className='text-center py-10'>
+                        <Link to={`/checkout/${courses.id}`} className='btn btn-success'> Premium Access</Link>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
